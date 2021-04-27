9eb5b45133f0c8223ad9af3eb5a3d2be27372325
Duwei1993 <1209772556@qq.com>
2021-04-27 11:21:52 +0800

    commit Knight class

---
+++ src/main/java/com/springinaction/knights/Knight.java
@@ -0,0 +1,13 @@
+package com.springinaction.knights;
+
+/**
+ * @author DW
+ * @version 1.0
+ * @date 2021/4/27 11:08
+ **/
+public interface Knight {
+    /**
+     *所有骑士都能接受请求
+     */
+    void embarkOnQuest();
+}
---
+++ src/main/java/com/springinaction/knights/imp/DamselRescuingKnight.java
@@ -0,0 +1,15 @@
+package com.springinaction.knights.imp;
+
+import com.springinaction.knights.Knight;
+
+/**
+ * @author DW
+ * @version 1.0
+ * @date 2021/4/27 11:19
+ **/
+public class DamselRescuingKnight implements Knight {
+    @Override
+    public void embarkOnQuest() {
+
+    }
+}
